c80fdcdde9d0732e4036d9a89487fa4177cc57c5
acyutaharidas-gif <acyutaharidas@gmail.com>
2026-02-24 17:31:41 +0000

    too many comments

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -25,24 +25,38 @@ pragma solidity ^0.8.0;
 /**
  * @title A sample Raffle Contract
  * @author Acyuta
- * @notice This contract is for creating a sample raffle
- * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2
  */
 
 contract Raffle {
+    /* Errors */
+    error Raffle__NotEnoughETHSent();
+    
+    /* Variables */
     uint256 private immutable i_entranceFee;
 
-    constructor(uint256 entranceFee){
+    /* Constructor */
+    constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
-    
-    function enterRaffle() public payable {}
-    
+
+    /* Receive Function */
+    /* Fallback Function */
+
+    /* External Functions */
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        if(msg.value >= i_entranceFee){
+            revert Raffle__NotEnoughETHSent();
+        }
+    }
+
+    /* Public Function */
     function pickWinner() public {}
 
-    /** Getter Functions */
-
-    function getEntraceFee() external view returns(uint256) {
+    /* Getter Functions */
+    function getEntraceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
